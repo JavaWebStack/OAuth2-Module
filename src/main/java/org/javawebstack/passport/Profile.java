ebfb7970eed3f6872d9ddc987ea6477d59c1033e
Julian Gojani <julian@gojani.xyz>
2021-03-14 16:50:52 +0100

    Profile extends now AbstractData to get more information if wanted

--- src/main/java/org/javawebstack/passport/Profile.java
+++ src/main/java/org/javawebstack/passport/Profile.java
@@ -7,4 +7,20 @@ public class Profile extends AbstractObject {
     public String name;
     public String mail;
     public String avatar;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
 }
